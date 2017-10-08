eacd6de70d3872de3e865dd8b1a1b82d12b3cd0d
Brian Coveney <coveneybrian@gmail.com>
2017-10-08 16:15:01 +0100

    Drop row(s) if null in the age col

--- Main.py
+++ Main.py
@@ -5,23 +5,41 @@ import matplotlib.pyplot as plt
 import tkinter
 
 
-# Solution to Q1 (ii) + (iii)
+# Solution to Q1 (ii)
 def compare_ages(data):
     print("Mean age of all respondents ", np.mean(data["age"]))
     print("Standard deviation of age for all respondents ", np.std(data["age"]))
 
 
-# Solution to Q1 (iv)
+# Solution to Q1 (iii)
 def avg_age_with_earnings_over_50k(data):
     print(data.groupby("Income")["age"].mean())
 
 
-# Solution to Q1 (v)
+# Solution to Q1 (iv)
 def top_three_occupations(data):
     print("Female:\n", data.loc[data.sex=="Female", "occupation"].value_counts().head(4))
     print("Male:\n", data.loc[data.sex=="Male", "occupation"].value_counts().head(4))
 
     # print(data.groupby("sex")["occupation"].value_counts())
+
+
+def gen_histogram(data):
+
+    # Grab DataFrame rows where specific column is null/notnull- prints 32561
+    data_copy = data[data['age'].isnull()]
+    print(data_copy["age"])
+
+    # Create a new dataframe, dropping the row with a null value for age
+    new_df = data.drop(data.index[32561])
+    df = new_df[new_df['age'].isnull()]
+
+    # Confirm row has been dropped
+    print('Empty', df['age'])
+
+    # Generate a histogram using Seaborn
+    sns.boxplot(x=data["age"])
+    plt.show()
 
 
 def main():
@@ -35,6 +53,9 @@ def main():
 
     # Q1 (v)
     top_three_occupations(df)
+
+    # Q1 (vi)
+    gen_histogram(df)
 
 
 main()
